61d91cc26d81e9d1e41c7ed03e5a8f172e24b745
komod0 <juliancrespo15@gmail.com>
2020-05-02 20:34:16 -0300

    precarious main

---
+++ main.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int stdin_select(FILE* input, int argc, const char* argv[]) {
+  int exit_code = 0;
+
+  if (argc == 4) {
+    input = fopen(argv[3], "r");
+    if (input == NULL) {
+      fprintf(stderr, "Error al abrir el archivo\n");
+      exit_code = 1;
+    }
+  } else if(argc == 3) {
+    input = stdin;
+  } else {
+    printf("Cantidad de argumentos incorrecta\n");
+    exit_code = 1;
+  }
+
+  return exit_code;
+}
+
+int main(int argc, const char* argv[]) {
+  FILE* input;
+  if (stdin_select(input, argc, argv) == 1) return 1;
+  
+  return 0;
+}
